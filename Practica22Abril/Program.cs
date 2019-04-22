9f4150b990132580f113d5c6458ffa51bfb1c530
Marco Torres <a01568181@itesm.mx>
2019-04-22 12:41:17 -0600

    Practica de 22 de abril del 2019

---
+++ Practica22Abril/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica22Abril
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
